02cab03680a9ad1e57a78fea382197387f8d70e6
Harun Gültekin <harungultekin05@gmail.com>
2023-04-16 21:39:57 +0300

    CharacterController
    Animations, model and character controller updated

---
+++ Assets/Scripts/Player/CameraManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform targetTransform; // The object the camera will follow
+    public Transform cameraPivot;
+    private Vector3 cameraFollowVelocity = Vector3.zero;
+    public GameObject player;
+
+    public float cameraDelay = 0.1f;
+    public float cameraLookSpeed = 2;
+    public float cameraPivotSpeed = 2;
+
+    public float lookAngle;
+    public float pivotAngle;
+    public float minimumPivotAngle = -35;
+    public float maximumPivotAngle = 35;
+
+    private void Awake()
+    {
+        targetTransform = player.transform;
+    }
+
+
+    public void FollowTarget()
+    {
+        Vector3 targetPosition = Vector3.SmoothDamp(transform.position, targetTransform.position, ref cameraFollowVelocity, cameraDelay);
+        transform.position = targetPosition;
+
+    }
+
+    public void RoteteCamera()
+    {
+
+        float mouseXInput = Input.GetAxis ("Mouse X");
+        float mouseYInput = Input.GetAxis ("Mouse Y");
+        //Debug.Log("mouseXInput:"+mouseXInput+" mouseYInput:"+mouseYInput);
+        lookAngle += mouseXInput * cameraLookSpeed;
+        pivotAngle -= mouseYInput * cameraPivotSpeed;
+        pivotAngle = Mathf.Clamp(pivotAngle, minimumPivotAngle, maximumPivotAngle);
+
+        Vector3 rotation = Vector3.zero;
+        rotation.y = lookAngle;
+        Quaternion targetRotation = Quaternion.Euler(rotation);
+        transform.rotation = targetRotation;
+
+        rotation = Vector3.zero;
+        rotation.x = pivotAngle;
+        targetRotation = Quaternion.Euler(rotation);
+        cameraPivot.localRotation = targetRotation;
+
+
+    }
+
+    private void LateUpdate()
+    {
+        FollowTarget();
+        RoteteCamera();
+    }
+}
--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -5,14 +5,23 @@ using UnityEngine.AI;
 
 public class PlayerScript : MonoBehaviour
 {
-    public float speed;
+    public float movementSpeed;
+    public float rotationSpeed;
     private Animator playerAnimator;
     public float movementVerticalInput;
     public float movementHorizontalInput;
     public float movementJumpInput;
     private Rigidbody selfRigidbody;
     public bool isGrounded;
+    public bool JumpAction;
     public int jumpHeight = 150;
+    private float keepY;
+    enum JumpState    {Ground, JumpPrepare, Jumping, Landing}
+    JumpState jState = JumpState.Ground;
+
+     // Camera variables
+    public Transform cameraTransform;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,37 +33,127 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         Jump();
-        Move();
+        if (jState == JumpState.Ground) Move();
+        if (jState == JumpState.Ground) Rotate();
     }
 
     private void Move()
     {
+
         // Get movement inputs from Input Manager
         movementVerticalInput = Input.GetAxis("Vertical");
         movementHorizontalInput = Input.GetAxis("Horizontal");
         // Take first position of object as Vector3 beginning of the frame and set new position according to movement inputs and set this Vector3 as new position
         Vector3 currentPosition = transform.position;
-        currentPosition.z += movementVerticalInput * speed * Time.deltaTime;
-        currentPosition.x += movementHorizontalInput * speed * Time.deltaTime;
-        transform.position = currentPosition;
+        Vector3 movePosition = Vector3.zero;
+        movePosition += cameraTransform.forward * movementVerticalInput * movementSpeed * Time.deltaTime;
+        movePosition += cameraTransform.right * movementHorizontalInput * movementSpeed * Time.deltaTime;
+        //currentPosition += cameraTransform.forward * movementVerticalInput * movementSpeed * Time.deltaTime;
+        //currentPosition += cameraTransform.right * movementHorizontalInput * movementSpeed * Time.deltaTime;
+        movePosition.y = 0;
+        transform.position = currentPosition + movePosition;
 
         // Animation play according to movement input, animation transitions handled by "move" and "jump" parameters which set in Animator. 0.1 is threshold for animation plays.
-        if ((movementVerticalInput != 0 || movementHorizontalInput != 0) && isGrounded) playerAnimator.SetFloat("move", 0.2f);
-        else playerAnimator.SetFloat("move", 0.0f);
+        if ((movementVerticalInput != 0 || movementHorizontalInput != 0) && isGrounded) playerAnimator.SetBool("Walk", true);
+        else playerAnimator.SetBool("Walk", false);
 
+    }
+
+    IEnumerator DelayJumpPrepare(float _delay = 0)
+    {
+        yield return new WaitForSeconds(_delay);
     }
 
     private void Jump()
     {
-        // Space Jump input from Input Manager, when it is pressed and character is on Ground, play Jump animation and add force in y-axis.
+        //Debug.Log(jState);
         movementJumpInput = Input.GetAxis("Jump");
-        if (isGrounded && (movementJumpInput != 0))
+        switch(jState)
         {
-            playerAnimator.SetFloat("jump", 0.2f);
-            selfRigidbody.AddForce(Vector3.up * jumpHeight);
+            case JumpState.Ground:
+            {
+                if (movementJumpInput != 0 && isGrounded)
+                {
+                    playerAnimator.SetBool("Jump", true);
+                    playerAnimator.SetBool("Walk", false);
+                    playerAnimator.SetBool("JumpRelease", false);
+                    playerAnimator.SetBool("Land", false);
+                    jState = JumpState.JumpPrepare;
+                }
+                break;
+            }
+            case JumpState.JumpPrepare:
+            {
+                if (movementJumpInput == 0 && playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("JumpPrepare"))
+                {
+                    playerAnimator.SetBool("Jump", true);
+                    playerAnimator.SetBool("Walk", false);
+                    playerAnimator.SetBool("JumpRelease", true);
+                    playerAnimator.SetBool("Land", false);
+                    movementVerticalInput = Input.GetAxis("Vertical");
+                    movementHorizontalInput = Input.GetAxis("Horizontal");
+                    Vector3 JumpForce = Vector3.zero;
+                    JumpForce += cameraTransform.forward * movementVerticalInput * jumpHeight/2;
+                    JumpForce += cameraTransform.right * movementHorizontalInput * jumpHeight/2;
+                    JumpForce.y = 0;
+                    JumpForce += Vector3.up * jumpHeight;
+                    selfRigidbody.AddForce(JumpForce);
+                    jState = JumpState.Jumping;
+                }
+                break;
+            }
+            case JumpState.Jumping:
+            {
+                if (!isGrounded) JumpAction = true;
+                StartCoroutine(DelayJumpPrepare(1.0f));
+                if (isGrounded && JumpAction && playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+                {
+                    playerAnimator.SetBool("Jump", false);
+                    playerAnimator.SetBool("Walk", false);
+                    playerAnimator.SetBool("JumpRelease", false);
+                    playerAnimator.SetBool("Land", true);
+                    jState = JumpState.Landing;
+                }
+                break;
+            }
+            case JumpState.Landing:
+            {
+                if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Land"))
+                {
+                    playerAnimator.SetBool("Jump", false);
+                    playerAnimator.SetBool("Walk", false);
+                    playerAnimator.SetBool("JumpRelease", false);
+                    playerAnimator.SetBool("Land", false);
+                    JumpAction = false;
+                    jState = JumpState.Ground;
+                }
+                break;
+            }
+                default: break;
         }
 
-        else playerAnimator.SetFloat("jump", 0.0f);
+        // Space Jump input from Input Manager, when it is pressed and character is on Ground, play Jump animation and add force in y-axis.
+        //else playerAnimator.SetBool("Jump", false);
+    }
+
+    private void Rotate()
+    {
+        Vector3 targetDirection = Vector3.zero;
+
+        movementVerticalInput = (-1)*Input.GetAxis("Vertical");
+        movementHorizontalInput = Input.GetAxis("Horizontal");
+        targetDirection = cameraTransform.forward * movementHorizontalInput;
+        targetDirection += cameraTransform.right * movementVerticalInput;
+        targetDirection.y = 0;
+
+        if (targetDirection == Vector3.zero)
+        {
+            targetDirection = transform.forward;
+        }
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = playerRotation;
     }
 
     private void OnCollisionEnter(Collision other)
@@ -63,7 +162,6 @@ public class PlayerScript : MonoBehaviour
     if(other.gameObject.tag == "Ground")
         {
             isGrounded = true;
-            playerAnimator.SetFloat("jump", 0.0f);
         }
     }
     void OnCollisionExit(Collision other)
@@ -72,7 +170,7 @@ public class PlayerScript : MonoBehaviour
         if (other.gameObject.tag == "Ground")
         {
             isGrounded = false;
-            playerAnimator.SetFloat("jump", 0.0f);
+            //playerAnimator.SetFloat("jump", 0.0f);
         }
     }
 
