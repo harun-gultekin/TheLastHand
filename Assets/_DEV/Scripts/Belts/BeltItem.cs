0e8b38e96aa0f92c8cea505d7eea504e4a477612
Deniz Cakmak <berfz23@gmail.com>
2023-05-06 19:20:19 +0300

    fix

---
+++ Assets/_DEV/Scripts/Belts/BeltItem.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltItem : MonoBehaviour
+{
+   public GameObject item;
+
+   private void Awake()
+   {
+      item = gameObject;
+   }
+}
