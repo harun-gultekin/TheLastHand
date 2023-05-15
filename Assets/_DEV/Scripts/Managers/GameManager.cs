eee4ae3bd453b2e8d618f55329ab118cbdc69e08
Deniz Cakmak <berfz23@gmail.com>
2023-05-15 23:36:54 +0300

    edit

--- Assets/_DEV/Scripts/Managers/GameManager.cs
+++ Assets/_DEV/Scripts/Managers/GameManager.cs
@@ -2,21 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using LastHand;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject puzzlePrefab;
+    [SerializeField] private Button puzzleBackButton;
 
     private void OnEnable()
     {
         Events.GamePlay.OnMinimapCollider += OnMinimapCollider;
         Events.GamePlay.OnPuzzleWin += OnPuzzleWin;
+
+        puzzleBackButton.onClick.AddListener(() =>
+        {
+            puzzlePrefab.SetActive(false);
+        });
     }
     
     private void OnDisable()
     {
         Events.GamePlay.OnMinimapCollider -= OnMinimapCollider;
         Events.GamePlay.OnPuzzleWin -= OnPuzzleWin;
+        
+        puzzleBackButton.onClick.RemoveAllListeners();
     }
     
     private void OnMinimapCollider()
