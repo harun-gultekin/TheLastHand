99188923bc8bf7fae25629b2199e51a9ba4b094b
Harun Gültekin <harungultekin05@gmail.com>
2023-05-15 23:17:09 +0300

    PowerUps
    speed and jump powerups added

---
+++ Assets/_DEV/Scripts/PowerUpJump.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpJump : MonoBehaviour
+{
+    PlayerController pc = null;
+    private bool powerUpActive = false;
+
+    void Start()
+    {
+        pc = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !powerUpActive)
+        {
+            powerUpActive = true;
+            StartCoroutine(PowerUpWearOff(5));
+            powerUpActive = false;
+        }
+    }
+
+    IEnumerator PowerUpWearOff(float waitTime)
+    {
+        int jump = pc.jumpHeight;
+        pc.jumpHeight = pc.jumpHeight * 2;
+        yield return new WaitForSeconds(waitTime);
+        pc.jumpHeight = jump;
+    }
+}
