26132090062df55fa2fe9a6b1f2b4f286c902675
Harun Gültekin <harungultekin05@gmail.com>
2023-06-11 20:29:46 +0300

    Press Machine Prefab Created
    For trigger move of press machine, under prefab there is gameObject name is moveablePart, it has a script. Public bool variable triggerActive should be true for move action, defaul value is false

--- Assets/PressMachineManager.cs
+++ Assets/PressMachineManager.cs
@@ -6,17 +6,26 @@ using UnityEngine;
 public class PressMachineManager : MonoBehaviour
 {
     [SerializeField] private GameObject moveMesh;
+    [SerializeField] private float startPoint = 2.527f;
+    [SerializeField] private float endPoint = 0.167f;
+    [SerializeField] private float slowness = 3f;
+    private bool animationStarted = false;
+    [SerializeField] public bool triggerActive = false;
 
-    private void Start()
+    private void Update()
     {
-        SetAnimation();
+        if (!animationStarted && triggerActive)
+        {
+            SetAnimation();
+            animationStarted = true;
+        }
     }
 
     private void SetAnimation()
     {
-        moveMesh.transform.DOLocalMoveZ(0.26f, 1f).OnComplete(() =>
+        moveMesh.transform.DOLocalMoveY(startPoint, slowness).OnComplete(() =>
         {
-            moveMesh.transform.DOLocalMoveZ(-0.4f, 1f).OnComplete(SetAnimation);
+            moveMesh.transform.DOLocalMoveY(endPoint, slowness).OnComplete(SetAnimation);
         });
     }
 }
