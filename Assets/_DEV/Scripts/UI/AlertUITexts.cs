2f16ef6c4e7081c10692bc538bf74340238fff36
Deniz Cakmak <berfz23@gmail.com>
2023-06-12 02:33:29 +0300

    fix

--- Assets/_DEV/Scripts/UI/AlertUITexts.cs
+++ Assets/_DEV/Scripts/UI/AlertUITexts.cs
@@ -3,7 +3,7 @@ public static class AlertUITexts
     public const string GET_MINIMAP = "Solve the puzzle to get the minimap!";
     public const string HIDE_AGENTS = "Use boxes and drawers to hide from agents! Press E for both!!";
     public const string TURN_VALVE = "Press E for turn valve to hide from agents!"; 
-    public const string GO_CONTROL = "Go control panel!";
+    public const string GO_CONTROL = "Go control panelfor lock the doors!";
     public const string USE_DRAWER = "Press E for hide in drawer!";
     public const string CLOSE_DRAWER = "Press E for close drawer!";
 }
